a29761b96fefc185ff740e54ecec2154e27685ae
laiba-javaid <laibajavaid423@gmail.com>
2023-06-20 17:20:21 +0500

    message

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome to the Little Lemon App!</Text>
+      <Text>Welcome to the Little Lemon App!!</Text>
       <StatusBar style="auto" />
     </View>
   );
